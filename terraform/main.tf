7b3f0515162c9e22983317b739300a277c098756
Diego Garcia <44004958+diegog@users.noreply.github.com>
2024-04-25 16:00:26 -0600

    Terraform code for azure document storage (#3)
    Adds terraform to create a resource group, storage account, and a storage container within that account.

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+data "azurerm_client_config" "current" {}
+
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "random_string" "storage_account_name" {
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "${random_string.storage_account_name.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "documents" {
+  name                  = "document-container"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "blob"
+}
